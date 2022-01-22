7f58391700001789183ddafae5510e3f67967bc7
Cristian Burile <cristianburile@hotmail.com>
2022-01-22 19:09:29 -0300

    Finish 1329 e 2060

---
+++ src/AdHoc/Uri1329/Uri.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Uri1329
+{
+    public static class Uri
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                if (int.Parse(Console.ReadLine()) == 0)
+                    return;
+
+                var convertAll = Console.ReadLine().Split(' ').Where(s => !string.IsNullOrEmpty(s)).Select(int.Parse);
+
+                var mary = 0;
+                var john = 0;
+
+                foreach (var i in convertAll)
+                    if (i == 0)
+                        mary++;
+                    else
+                        john++;
+
+                Console.WriteLine($"Mary won {mary} times and John won {john} times");
+            }
+        }
+    }
+}
---
+++ src/Beginner/Uri2060/Uri.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Uri2060
+{
+    public static class Uri
+    {
+        public static void Main()
+        {
+            var _ = Console.ReadLine();
+
+            var dictionary = new Dictionary<int, int>()
+            {
+                {2, 0},
+                {3, 0},
+                {4, 0},
+                {5, 0},
+            };
+
+            var convertAll = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            foreach (var i in convertAll)
+            {
+                if (i % 2 == 0)
+                    dictionary[2]++;
+                if (i % 3 == 0)
+                    dictionary[3]++;
+                if (i % 4 == 0)
+                    dictionary[4]++;
+                if (i % 5 == 0)
+                    dictionary[5]++;
+            }
+
+            foreach (var i in dictionary)
+                Console.WriteLine($"{i.Value} Multiplo(s) de {i.Key}");
+        }
+    }
+}
