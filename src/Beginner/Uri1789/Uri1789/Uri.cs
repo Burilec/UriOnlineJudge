0eae2074a72eb9bf8bda80a9ec61586fdf872eb5
Cristian Burile <cristianburile@hotmail.com>
2022-01-22 11:56:25 -0300

    Finish 1789

---
+++ src/Beginner/Uri1789/Uri1789/Uri.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Uri1789
+{
+    public static class Uri
+    {
+        public static void Main()
+        {
+            while (Console.ReadLine() != null)
+            {
+                var max = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse).Max();
+
+                if (max >= 20)
+                    Console.WriteLine(3);
+                else if (max >= 10)
+                    Console.WriteLine(2);
+                else
+                    Console.WriteLine(1);
+            }
+        }
+    }
+}
