644649e770ca68e412565d71cf0801c9552e6ff9
Cristian Burile <cristianburile@hotmail.com>
2022-01-22 16:18:04 -0300

    Finish 1171

---
+++ src/AdHoc/Uri1171/Uri.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Uri1171
+{
+    public static class Uri
+    {
+        public static void Main()
+        {
+            var total = int.Parse(Console.ReadLine());
+
+            var dictionary = new Dictionary<int, int>();
+
+            for (var i = 0; i < total; i++)
+            {
+                var value = int.Parse(Console.ReadLine());
+
+                if (dictionary.ContainsKey(value))
+                    dictionary[value]++;
+                else
+                    dictionary.Add(value, 1);
+            }
+
+            foreach (var keyValuePair in dictionary.OrderBy(pair => pair.Key))
+                Console.WriteLine($"{keyValuePair.Key} aparece {keyValuePair.Value} vez(es)");
+        }
+    }
+}
