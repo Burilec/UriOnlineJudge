fa044b0368d27c9fbc2ca635bb41cabda7867ade
Cristian Burile <cristianburile@hotmail.com>
2022-01-22 12:42:51 -0300

    Finish 1087

---
+++ src/AdHoc/Uri1087.Tests/UriTest.cs
@@ -0,0 +1,26 @@
+using DefaultTest;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Uri1087.Tests
+{
+    public sealed class UriTest
+    {
+        [Theory]
+        [InlineData("testInput1.txt", "testResult1.txt")]
+        [InlineData("testInput2.txt", "testResult2.txt")]
+        [InlineData("testInput3.txt", "testResult3.txt")]
+        [InlineData("testInput4.txt", "testResult4.txt")]
+        public async Task Test(string fileNameInput, string fileNameResult)
+        {
+            var input = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "Input", fileNameInput);
+
+            var outPut = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "Result", fileNameResult);
+
+            Assert.True(await Default.Test(input, outPut, Uri.Main));
+        }
+    }
+}
---
+++ src/AdHoc/Uri1087/Uri.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Uri1087
+{
+    public static class Uri
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                var convertAll = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+                if (convertAll[0] == 0)
+                    return;
+
+                var queenX = convertAll[0];
+                var queenY = convertAll[1];
+                var placeX = convertAll[2];
+                var placeY = convertAll[3];
+
+                if (queenX == placeX && placeY == queenY)
+                    Console.WriteLine(0);
+                else if (queenY + queenX == placeY + placeX || Math.Abs(queenX - placeX) == Math.Abs(queenY - placeY) || placeY == queenY || placeX == queenX)
+                    Console.WriteLine(1);
+                else
+                    Console.WriteLine(2);
+            }
+        }
+    }
+}
