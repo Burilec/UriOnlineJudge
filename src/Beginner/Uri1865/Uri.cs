41d0a558dd78a351cf4059adf05017f4187f7bf5
Cristian Burile <cristianburile@hotmail.com>
2022-01-22 18:26:22 -0300

    Finish 1865

---
+++ src/Beginner/Uri1865/Uri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uri1865
+{
+    public static class Uri
+    {
+        public static void Main()
+        {
+            var total = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i < total; i++)
+                Console.WriteLine(Console.ReadLine().Split(' ')[0] == "Thor" ? "Y" : "N");
+        }
+    }
+}
