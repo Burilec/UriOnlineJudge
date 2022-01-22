d17167d1e700ba1913f03a432cd8bc3ef1ed9fbf
Cristian Burile <cristianburile@hotmail.com>
2022-01-22 18:09:23 -0300

    Finish 1865

---
+++ src/Beginner/Uri2006/Uri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Uri2006
+{
+    public static class Uri
+    {
+        public static void Main()
+        {
+            var target = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse).Count(i => i == target));
+        }
+    }
+}
