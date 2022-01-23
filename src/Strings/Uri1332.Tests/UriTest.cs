f5ef6fd4f8c6d611fbc12b98788d71aecc567c21
Cristian Burile <cristianburile@hotmail.com>
2022-01-22 23:57:14 -0300

    Finish 1332

---
+++ src/Strings/Uri1332.Tests/UriTest.cs
@@ -0,0 +1,27 @@
+using DefaultTest;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Uri1332.Tests
+{
+    public sealed class UriTest
+    {
+        [Theory]
+        [InlineData("testInput1.txt", "testResult1.txt")]
+        [InlineData("testInput2.txt", "testResult2.txt")]
+        [InlineData("testInput3.txt", "testResult3.txt")]
+        [InlineData("testInput4.txt", "testResult4.txt")]
+        [InlineData("testInput5.txt", "testResult5.txt")]
+        [InlineData("testInput6.txt", "testResult6.txt")]
+        public async Task Test(string fileNameInput, string fileNameResult)
+        {
+            var input = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "Input", fileNameInput);
+
+            var outPut = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "Result", fileNameResult);
+
+            Assert.True(await Default.Test(input, outPut, Uri.Main));
+        }
+    }
+}
---
+++ src/Strings/Uri1332/Uri.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Uri1332
+{
+    public static class Uri
+    {
+        public static void Main()
+        {
+            var total = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i < total; i++)
+            {
+                var value = Console.ReadLine();
+
+                if (value.Length == 3)
+                    Console.WriteLine(CheckOne(value) ? 1 : 2);
+                else
+                    Console.WriteLine(3);
+            }
+        }
+
+        private static bool CheckOne(string value)
+        {
+            var one = 0;
+
+            if (value[0] == 'o') one++;
+            if (value[1] == 'n') one++;
+            if (one == 2)
+                return true;
+
+            if (value[2] == 'e') one++;
+
+            return one == 2;
+        }
+    }
+}
